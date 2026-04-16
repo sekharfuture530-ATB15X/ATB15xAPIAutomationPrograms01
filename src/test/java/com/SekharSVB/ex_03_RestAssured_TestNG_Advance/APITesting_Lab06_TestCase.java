821dce6e8a2bf6279df0322176d0d8fc8f7885dd
Sekhar V <sekharfurture530@gmail.com>
2026-04-16 19:12:26 +0530

    API Testcase with BDD Style and NON BDD Style

---
+++ src/test/java/com/SekharSVB/ex_03_RestAssured_TestNG_Advance/APITesting_Lab06_TestCase.java
@@ -0,0 +1,50 @@
+package com.SekharSVB.ex_03_RestAssured_TestNG_Advance;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class APITesting_Lab06_TestCase {
+
+    String pincode = "110048";
+
+    @Test
+    public void test_GET_POSITIVE_Tc1(){
+
+        pincode ="110048";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/"+pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
+
+    @Test
+    public void test_GET_NEGATIVE_TC2(){
+        pincode = "@";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
+    @Test
+    public void test_GET_NEGATIVE_TC3(){
+        pincode = " ";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/USA/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+
+    }
+
+}
